c2bf388c722315194701567bee31aa8b221325d9
Alexandr <skripkin.offical@mail.ru>
2023-06-24 15:40:03 +0300

    решение дз

---
+++ Home_work/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count <= num)
+{
+System.Console.Write(count + " ");
+count += 2;
+}
